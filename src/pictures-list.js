437fdf372ba88ebb534f83d56e07dcacb15bc42a
Mary-Mei <109496345+Mary-Mei@users.noreply.github.com>
2022-12-23 19:36:16 +1000

    Update pictures-list.js

--- src/pictures-list.js
+++ src/pictures-list.js
@@ -1,18 +1,38 @@
+const previewModalElement = document.querySelector('.big-picture');
+const previewModalCloseBtnElement = document.querySelector('.big-picture__cancel');
+
 const picturesListElement = document.querySelector('.pictures');
 const pictureTempLateElement = document.getElementById('picture')
     .content
     .querySelector('.picture');
 
-
 const renderPicturesList = (pictures) => {
     for (const picture of pictures) {
         const pictureElement = pictureTempLateElement.cloneNode(true);
 
+        pictureElement.dataset.id = picture.id;
+
         pictureElement.querySelector('img').src = picture.url;
-        // pictureElement.querySelector('.card-title').textContent = picture.description;
+        pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+        pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
 
         picturesListElement.append(pictureElement);
+
+        pictureElement.addEventListener('click', function () {
+            const id = pictureElement.dataset.id;
+
+            const pictureItem = pictures.find(function (picture) {
+                return picture.id === Number(id)
+            })
+            
+            previewModalElement.querySelector('img').src = pictureItem.url;
+            previewModalElement.classList.remove('hidden');
+        });
     }
+
+    previewModalCloseBtnElement.addEventListener('click', function () {
+        previewModalElement.classList.add('hidden');
+    });
 }
 
 export {renderPicturesList};
